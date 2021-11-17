f6ef2708d81e171be3195558fe88b3787b41d00e
Francesco-19 <didonnafrancescomaria@gmail.com>
2021-11-17 17:07:18 +0100

    ho cambiato il file goals che era scomparso in goals_view

---
+++ Bullet_V1/Goals_View.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Goals: View {
+    var body: some View {
+        Text("Goals")
+    }
+}
+
+struct Goals_Previews: PreviewProvider {
+    static var previews: some View {
+        Goals()
+    }
+}
